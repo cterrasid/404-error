6ec04c10813bad32ba6c8f42a48e9a091231cf41
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-09-15 15:36:48 +0200

    Add image of the dark side of the moon and improve styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
     <div class="container">
-      <section class="image">Image</section>
-      <section class="text">
-        <h1>¡Ay no!</h1>
-        <p class="message">Has llegado a la cara "oculta" de la luna ¿Qué haces aquí?. En este lugar no existen webs <em>por ahora</em>, así que puedes:</p>
-        <div class="buttons">
-          <button>Ojo</button>
-          <button>Volver a casa</button>
-        </div>
-      </section>
+      <h1 class="title">404 Not Found</h1>
+      <div class="content">
+        <section class="image">
+          <img src="./assets/moon.png" alt="Far side of the moon" />
+        </section>
+        <section class="text">
+          <h2>¡Ay no! ¿Qué haces aquí?</h2>
+          <p class="message">
+            Has llegado a la cara
+            <strong>oculta</strong> de la luna. En este lugar no existen páginas
+            <em>por ahora</em>, así que puedes:
+          </p>
+          <div class="buttons">
+            <button>Ojo</button>
+            <button>Volver a casa</button>
+            <button>¡Avísame!</button>
+          </div>
+        </section>
+      </div>
     </div>
   </div>
 </template>
@@ -31,11 +41,60 @@ export default {
   -webkit-font-smoothing: antialiased;
 }
 
+.title {
+  align-self: flex-start;
+  font-family: monospace;
+  font-size: 12px;
+  text-transform: uppercase;
+  width: 100%;
+}
+
 .container {
-  background-color: plum;
-  height: 100vh;
-  display: flex;
   align-items: center;
+  background-color: black;
+  color: beige;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
   justify-content: space-evenly;
+  padding: 20px;
+}
+
+.content {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+}
+
+.image img {
+  height: 100px;
+}
+
+.text h2 {
+  text-align: center;
+}
+
+.buttons {
+  display: flex;
+  justify-content: space-evenly;
+  margin: 20px;
+}
+
+@media screen and (min-width: 768px) {
+  .content {
+    flex-direction: row;
+  }
+
+  .text h2 {
+    text-align: start;
+  }
+
+  .buttons {
+    justify-content: space-between;
+    margin: 15px 0;
+    width: 200px;
+  }
 }
 </style>
